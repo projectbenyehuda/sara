d816993efa1cee1b5030ce7adf62a84a1f94ff68
Asaf Bartov <asaf.bartov@gmail.com>
2022-10-31 01:50:03 +0300

    Destroy response items along with query

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,4 +1,4 @@
 class Query < ApplicationRecord
   validates :text, presence: true
-  has_many :response_items, inverse_of: :query
+  has_many :response_items, inverse_of: :query, dependent: :destroy
 end
